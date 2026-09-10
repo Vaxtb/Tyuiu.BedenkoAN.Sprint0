76f27da7b6de299c5d7c924f42f152473905b52a
Vaxtb <Bedenkoalex123@yandex.ru>
2026-09-10 16:22:29 +0500

    Task1

---
+++ Tyuiu.BedenkoAN.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.BedenkoAN.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
