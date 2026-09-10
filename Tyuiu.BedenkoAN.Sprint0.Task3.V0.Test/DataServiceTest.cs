39b1afce181d030369ede6cb0bc1cf1de7ad999e
Vaxtb <Bedenkoalex123@yandex.ru>
2026-09-10 19:15:40 +0500

    Task3

---
+++ Tyuiu.BedenkoAN.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BedenkoAN.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.BedenkoAN.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+
+
+        }
+    }
+}
