9a0b68215ab95d559ccea5290cf9b9b5740a4f7b
Vaxtb <Bedenkoalex123@yandex.ru>
2026-09-12 00:08:26 +0500

    Task5

---
+++ Tyuiu.BedenkoAN.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.BedenkoAN.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.BedenkoAN.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        
+        [TestMethod]
+        public void CheckedAdditionValid()
+
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+
+
+        }
+        [TestMethod]
+
+        public void CheckedSubstractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+
+
+        }
+        [TestMethod]
+
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(5, 10));
+
+
+        }
+        [TestMethod]
+
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+
+        }
+    }
+}
