f1c1eba72dcfb4f0d7a77f3e75226933db564be8
Vaxtb <Bedenkoalex123@yandex.ru>
2026-09-12 18:01:01 +0500

    Task6

---
+++ Tyuiu.BedenkoAN.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BedenkoAN.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.BedenkoAN.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма элементов массива =" + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Разность элементов массива =" + DataService.SubstractionArray(arraynums));
+            Console.WriteLine("Произведение элементов массива =" + DataService.MultiplicationArray(arraynums));
+            Console.ReadKey();
+        }
+    }
+}
